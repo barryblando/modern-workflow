d3aabe49ac58403ef25b105bc20845ceae0ba85c
heartless007 <barryblando@gmail.com>
2017-11-30 22:02:32 +0800

    Added Modules and Partials

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,27 @@
 import G from 'gulp';
+import PC from 'gulp-postcss';
+import AP from 'autoprefixer';
+import pcSV from 'postcss-simple-vars';
+import pcNested from 'postcss-nested';
+import CI from 'postcss-import';
+import GW from 'gulp-watch';
 
-const [gulp] = [G];
+const [gulp, postcss, autoprefixer, cssvars, nested, cssImport, watch] = [G, PC, AP, pcSV, pcNested, CI, GW];
 
-gulp.task('default', function() {
+gulp.task('default', () => {
   console.log('Hooray');
 });
 
-gulp.task('html', function() {
+gulp.task('html', () => {
   console.log('Imagine');
+});
+
+gulp.task('styles', () => gulp.src('./app/assets/styles/style.css')
+  .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+  .pipe(gulp.dest('./app/_build/styles')));
+
+gulp.task('watch', () => {
+  watch('./app/assets/styles/**/*.css', () => {
+    gulp.start('styles');
+  });
 });
