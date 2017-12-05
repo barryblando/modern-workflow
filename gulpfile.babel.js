433ac9a550e512813d71a5050030cc41aa6c7f5a
heartless007 <barryblando@gmail.com>
2017-12-05 18:29:18 +0800

    Done with the Headline Block

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,4 +1,2 @@
-import styles from './gulp/tasks/styles';
-import watch from './gulp/tasks/watch';
-// require('./gulp/tasks/styles');
-// require('./gulp/tasks/watch');
+import './gulp/tasks/styles';
+import './gulp/tasks/watch';
