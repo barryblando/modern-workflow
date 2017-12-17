df28b36287d8040532816c50b00553d42f85f617
heartless007 <barryblando@gmail.com>
2017-12-17 23:03:17 +0800

    Completed Nav Waypoints.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,10 +1,11 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 const mobileMenu = new MobileMenu();
 /* eslint-disable */
 new RevealOnScroll($('.feature-item'), '85%');
 new RevealOnScroll($('.testimonial'), '60%');
 /* eslint-enable */
-
+const stickyHeader = new StickyHeader();
