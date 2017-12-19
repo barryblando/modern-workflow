4272255485faf8269a6ccf34e4471b03333685ef
heartless007 <barryblando@gmail.com>
2017-12-19 22:12:07 +0800

    Completed the Modal Overlay.

--- app/_build/scripts/app.bundle.js
+++ app/_build/scripts/app.bundle.js
@@ -883,6 +883,10 @@ var _StickyHeader = __webpack_require__(8);
 
 var _StickyHeader2 = _interopRequireDefault(_StickyHeader);
 
+var _Modal = __webpack_require__(10);
+
+var _Modal2 = _interopRequireDefault(_Modal);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var mobileMenu = new _MobileMenu2.default();
@@ -891,6 +895,7 @@ new _RevealOnScroll2.default((0, _jquery2.default)('.feature-item'), '85%');
 new _RevealOnScroll2.default((0, _jquery2.default)('.testimonial'), '60%');
 /* eslint-enable */
 var stickyHeader = new _StickyHeader2.default();
+var modal = new _Modal2.default();
 
 /***/ }),
 /* 4 */
@@ -11351,7 +11356,8 @@ var StickyHeader = function () {
     key: 'createHeaderWaypoint',
     value: function createHeaderWaypoint() {
       /* eslint-disable */
-      var that = this; // points to main Class
+      // points to main Class
+      var that = this;
       // console.log(this.headerTriggerElement[0]);
       new Waypoint({
         // get the array-ish DOM element title
@@ -11773,6 +11779,75 @@ var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_
 }));
 
 
+
+/***/ }),
+/* 10 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Modal = function () {
+  function Modal() {
+    _classCallCheck(this, Modal);
+
+    this.openModalButton = (0, _jquery2.default)('.open-modal');
+    this.modal = (0, _jquery2.default)('.modal');
+    this.closeModalButton = (0, _jquery2.default)('.modal__close');
+    this.events();
+  }
+
+  _createClass(Modal, [{
+    key: 'events',
+    value: function events() {
+      // Never forget to bind the '.this' to main Class
+      // Clicking the open and close modal button
+      this.openModalButton.click(this.openModal.bind(this));
+      this.closeModalButton.click(this.closeModal.bind(this));
+
+      // pushes any key
+      (0, _jquery2.default)(document).keyup(this.keyPressHandler.bind(this));
+    }
+  }, {
+    key: 'keyPressHandler',
+    value: function keyPressHandler(e) {
+      if (e.keyCode === 27) {
+        this.closeModal();
+      }
+    }
+  }, {
+    key: 'openModal',
+    value: function openModal() {
+      this.modal.addClass('modal--is-visible');
+      // Don't let the user automatically scroll up to top of the page when it clicked a blank href link
+      // returning false will prevent that default behavior
+      return false;
+    }
+  }, {
+    key: 'closeModal',
+    value: function closeModal() {
+      this.modal.removeClass('modal--is-visible');
+    }
+  }]);
+
+  return Modal;
+}();
+
+exports.default = Modal;
 
 /***/ })
 /******/ ]);
